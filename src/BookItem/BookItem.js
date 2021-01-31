c0db49cc7b328e0db58409f4a38f70aa45d5c73b
Rae Schneider <rannschneid@gmail.com>
2021-01-31 11:29:42 -0700

    refactored BookshelfMain to use  BookItem component

--- src/BookItem/BookItem.js
+++ src/BookItem/BookItem.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import './BookItem.css';
-
+import AppContext from '../AppContext'
+import { Link } from 'react-router-dom'
+import { useContext } from 'react';
 
 function BookItem() {
+  const context = useContext(AppContext)
+  const bookshelf = context.bookshelf
+  const book = context.books
   return (
     <div className='BookItem'>
-      <h2>This is BookItem component.</h2>
+      {
+          context.bookshelf.map(book => ( 
+            <li key={book.id} style={{listStyle:'none'}}>
+              <p>Title: {book.title}</p>
+              <p>Author Last Name: {book.author_last}</p>
+              <p>Author First Name: {book.author_first}</p>
+              <p>Description: {book.description}</p>
+              <p>Category: {book.category}</p>
+              <p>Subcategory: {book.subcategory}</p>
+              {/* Link to updateBook form */}
+              <button><Link to={`/edit-book/${book.id}`}>Update Book</Link></button>
+              <button>Delete Book</button>
+              <hr />
+            </li>
+          )
+          )
+        }
+      
     </div>
   );
 }
--- src/BookshelfMain/BookshelfMain.js
+++ src/BookshelfMain/BookshelfMain.js
@@ -4,35 +4,35 @@ import './BookshelfMain.css';
 import { Link } from 'react-router-dom'
 import { useContext } from 'react';
 import AppContext from '../AppContext'
+import BookItem from '../BookItem/BookItem';
 
 function BookshelfMain(props) {
 
   const context = useContext(AppContext)
-  // console.log('context ', context.bookshelf)
-
-  // console.log('store ', STORE)
-  // console.log('props ', props)
   const bookshelf = context.bookshelf
+  const book = context.books
 
   return (
     <>
       <div className='Bookshelf__main'>
         <h2>Bookshelf</h2>
+        <BookItem />
         {
-          context.bookshelf.map(bookshelf => ( 
-            <li key={bookshelf.id} style={{listStyle:'none'}}>
-              <p>Title: {bookshelf.title}</p>
-              <p>Author Last Name: {bookshelf.author_last}</p>
-              <p>Author First Name: {bookshelf.author_first}</p>
-              <p>Description: {bookshelf.description}</p>
-              <p>Category: {bookshelf.category}</p>
-              <p>Subcategory: {bookshelf.subcategory}</p>
-              <button>Update</button>
-              <button>Delete</button>
-              <hr />
-            </li>
-          )
-          )
+          // context.bookshelf.map(bookshelf => ( 
+          //   <li key={bookshelf.id} style={{listStyle:'none'}}>
+          //     <p>Title: {bookshelf.title}</p>
+          //     <p>Author Last Name: {bookshelf.author_last}</p>
+          //     <p>Author First Name: {bookshelf.author_first}</p>
+          //     <p>Description: {bookshelf.description}</p>
+          //     <p>Category: {bookshelf.category}</p>
+          //     <p>Subcategory: {bookshelf.subcategory}</p>
+          //     {/* Link to updateBook form */}
+          //     <button><Link to={`/edit-book/${bookshelf.id}`}>Update Book</Link></button>
+          //     <button>Delete Book</button>
+          //     <hr />
+          //   </li>
+          // )
+          // )
         }
       </div>
       <div className='Add__book'> 
