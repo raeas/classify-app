aa456136dfd2b107b4622d9c0ebc609e29506eea
Rae Schneider <rannschneid@gmail.com>
2021-02-11 12:26:19 -0700

    udpates for vercel deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
-  // API_ENDPOINT: `http://localhost:8000/api/`,
+  API_ENDPOINT: `https://rocky-reaches-94023.herokuapp.com/api/`,
   API_KEY: process.env.REACT_APP_API_KEY,
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
-  "http://localhost:8000/api"
+  // API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
+  // "http://localhost:8000/api"
 }
 
